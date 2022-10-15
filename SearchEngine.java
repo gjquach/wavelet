896f5844e371cbb5633b63e534409b6ca519077d
gjquach <114312610+gjquach@users.noreply.github.com>
2022-10-14 17:25:40 -0700

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -35,9 +35,7 @@ class SearchEngine {
             System.out.println("Missing port number! Try any number between 1024 to 49151");
             return;
         }
-
         int port = Integer.parseInt(args[0]);
-
         Server.start(port, new Handler());
     }
 }
